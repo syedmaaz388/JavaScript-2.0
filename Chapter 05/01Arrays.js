5803b1b24d6fa159dddb303dfeaa2331c3413e6f
Syed Maaz <63224539+syedmaaz388@users.noreply.github.com>
2023-07-21 17:21:25 +0530

    Add files via upload

---
+++ Chapter 05/01Arrays.js
@@ -0,0 +1,18 @@
+const Fruits = ["Banana","Apple","Orange",null];
+// Accesing Value
+console.log(Fruits[0]); 
+console.log(Fruits[1]);
+console.log(Fruits[2]);
+
+// Finding length 
+console.log(Fruits.length);
+
+
+// Changing Value
+Fruits[4] = "Cherry";
+Fruits[3] = "Guava";
+console.log(Fruits);
+
+console.log(typeof Fruits); // Object
+
+// Arrays are mutable - It Can be Change
