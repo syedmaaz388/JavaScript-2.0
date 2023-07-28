f384b418727e7480d4f87d403c1a93b3e5d5a109
Syed Maaz <63224539+syedmaaz388@users.noreply.github.com>
2023-07-28 21:24:39 +0530

    Add files via upload

---
+++ Chapter 08/01InnerHtml_OuterHtml.js
@@ -0,0 +1,17 @@
+console.log(first.innerHTML);
+console.log(first.outerHTML);
+
+first.textContent = "Hello Maaz";
+console.log(first.textContent);
+// The innerHTML property sets or returns the HTML content (inner HTML) of an element.
+// The outerHTML property sets or returns the HTML element, including attributes, start tag, and end tag.
+// The textContent property sets or returns the text content of the specified node, and all its descendants.
+
+console.log(first.innerHTML);
+console.log(first.outerHTML);
+
+first.textContent += " Good Morning";
+
+
+
+
